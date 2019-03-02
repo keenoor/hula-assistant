69f65d8aedced22b612f35d2c1eecd49dc332386
chenliuchun <chen_liuchun@qq.com>
2019-03-02 20:44:53 +0800

    init

---
+++ src/main/java/com/keenor/hulaassistant/HulaAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.keenor.hulaassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulaAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HulaAssistantApplication.class, args);
+    }
+
+}
